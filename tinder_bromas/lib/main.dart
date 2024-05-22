0eba3f9ca6746251b3057788ce9a1ccccb27cdb3
= <=>
2024-05-22 20:47:16 +0200

    Pagina de chistes que tienen me gusta creado y ruta funcional

--- tinder_bromas/lib/main.dart
+++ tinder_bromas/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:tinder_bromas/megusta.dart';
+import 'megusta.dart';
 
 void main() {
   runApp(const MyApp());
@@ -13,70 +15,90 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(),
-      home: Scaffold(
-        appBar: AppBar(
-          title: Center(child: Text('API BROMAS')), // Centro el Titulo
-        ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      // Lógica para el botón de no me gusta
-                    },
-                    //Iconno de Dislike
-                    child: Icon(Icons.thumb_down),
-                  ),
-                  SizedBox(width: 10), // Espacio entre los botones
-                  Expanded(
-                    child: Center(
-                      //Centro el texto que recibi de la API
-                      child: FutureBuilder(
-                        future: fetchData(),
-                        builder:
-                            (BuildContext context, AsyncSnapshot snapshot) {
-                          if (snapshot.hasData) {
-                            return Text(snapshot.data);
-                          } else if (snapshot.hasError) {
-                            return Text("${snapshot.error}");
-                          }
-                          return CircularProgressIndicator();
-                        },
-                      ),
+      initialRoute: '/', // Esta es la ruta inicial de tu aplicación
+      routes: {
+        '/': (context) => HomePage(), // Esta es tu página de inicio
+        '/megusta': (context) =>
+            MegustaPage(), // Esta es la ruta a la página MegustaPage
+      },
+    );
+  }
+}
+
+//Conexión a la API de chistes y petición de datos
+Future<String> fetchData() async {
+  final response = await http.get(Uri.parse('https://icanhazdadjoke.com/'),
+      headers: {'Accept': 'text/plain'});
+
+  if (response.statusCode == 200) {
+    // Si todo va bien te devuelve el los datos en texto plano.
+    return response.body;
+  } else {
+    // Si va mal devuelve una excepción.
+    throw Exception('Failed to load data');
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Center(child: Text('API BROMAS')), // Centro el Titulo
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => MegustaPage()),
+                );
+              },
+              child: Text('Ir a Me Gusta'),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    // Lógica para el botón de no me gusta
+                  },
+                  //Iconno de Dislike
+                  child: Icon(Icons.thumb_down),
+                ),
+                SizedBox(width: 10), // Espacio entre los botones
+                Expanded(
+                  child: Center(
+                    //Centro el texto que recibi de la API
+                    child: FutureBuilder(
+                      future: fetchData(),
+                      builder: (BuildContext context, AsyncSnapshot snapshot) {
+                        if (snapshot.hasData) {
+                          return Text(snapshot.data);
+                        } else if (snapshot.hasError) {
+                          return Text("${snapshot.error}");
+                        }
+                        return CircularProgressIndicator();
+                      },
                     ),
                   ),
-                  SizedBox(width: 10), // Espacio entre los botones
-                  ElevatedButton(
-                    onPressed: () {
-                      // Lógica para el botón de me gusta
-                    },
-                    //Iconno de Like
-                    child: Icon(Icons.thumb_up),
-                  ),
-                ],
-              ),
-            ],
-          ),
+                ),
+                SizedBox(width: 10), // Espacio entre los botones
+                ElevatedButton(
+                  onPressed: () {
+                    // Lógica para el botón de me gusta
+                  },
+                  //Iconno de Like
+                  child: Icon(Icons.thumb_up),
+                ),
+              ],
+            ),
+          ],
         ),
       ),
     );
-  }
-
-//Conexión a la API de chistes y petición de datos
-  Future<String> fetchData() async {
-    final response = await http.get(Uri.parse('https://icanhazdadjoke.com/'),
-        headers: {'Accept': 'text/plain'});
-
-    if (response.statusCode == 200) {
-      // Si todo va bien te devuelve el los datos en texto plano.
-      return response.body;
-    } else {
-      // Si va mal devuelve una excepción.
-      throw Exception('Failed to load data');
-    }
   }
 }
---
+++ tinder_bromas/lib/megusta.dart
@@ -0,0 +1,16 @@
+// megusta.dart
+import 'package:flutter/material.dart';
+
+class MegustaPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Me Gusta'),
+      ),
+      body: Center(
+        child: Text('Esta es la página Me Gusta'),
+      ),
+    );
+  }
+}
